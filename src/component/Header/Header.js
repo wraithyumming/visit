43c7dbd2dda055ca4c399912b93048d1782be450
bladee <freedum0@gmail.com>
2025-04-16 18:57:40 +0300

    fix

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "./Header.css";
+import tgIcon from '../../assets/icons/tg50.png';
+import instIcon from '../../assets/icons/inst48.png';
 
 const Header = () => (
   <div className="menu">
-    <a href="#">Обо мне</a>
-    <a href="#">Услуги</a>
-    <a href="#">Портфолио</a>
-    <a href="#">Отзывы</a>
-    <a href="#">Гарантии</a>
+    <div className="menu-links">
+      <a href="#">Обо мне</a>
+      <a href="#">Услуги</a>
+      <a href="#">Портфолио</a>
+      <a href="#">Отзывы</a>
+      <a href="#">Гарантии</a>
+    </div>
 
-    <button className="btn">Связаться</button>
-  </div>
+      <div className="menu-actions">
+        <button className="btn">Связаться</button>
+           <a href="https://t.me/deuceich" target="_blank">
+              <img src={tgIcon} alt="Telegram" className="icon" />
+           </a>
+           <a href="https://www.instagram.com/" target="_blank">
+              <img src={instIcon} alt="Instagram" className="icon" />
+           </a>
+      </div>
+    </div>
 );
 
 export default Header;
