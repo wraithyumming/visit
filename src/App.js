925a21d68deeed576867a0962c3642c0b5dc4013
bladee <freedum0@gmail.com>
2025-05-03 21:47:47 +0300

    add new block

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Header from './component/Header/Header';
 import WelcomeBlock from './component/welcome/WelcomeBlock.js';
 import ServicesBlock from './component/Services/ServicesBlock.js';
 import Portfolio from './component/Portfolio/Portfolio.js';
-import ReviewsCarousel from './component/Review/ReviewsCarousel.js'
-
+import ReviewsCarousel from './component/Review/ReviewsCarousel.js';
+import Basement from './component/Basement/Basement.js';
 
 
 function App() {
   return (
     <div>
+
       <Header />
       <WelcomeBlock />
       <ServicesBlock />
       <Portfolio />
       <ReviewsCarousel />
+      <Basement />
+
     </div>
   )
 }
---
+++ src/component/Basement/Basement.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './Basement.css'
+
+const Basement = () => {
+    <div className="guarantees-block">
+        <h1 style={{ fontSize: "52px", paddingBottom: "20px" }}>ГАРАНТІЇ</h1>
+        <ol className="guarantees-points">
+  <li className="point">
+    Оплату приймаю через платіжну систему
+    <span style={{ color: "#4824ff" }}>Назва</span>,
+    яка контролює <br />безпечність грошових переказів.
+  </li>
+  <li className="point">
+    Переконатися в моїй відповідальності та професіоналізмі можна
+    <span style={{ color: "#4824ff" }}>написав клієнтам</span>,
+    <br />залишивши відгуки
+    <span style={{ color: "#4824ff" }}>особисто</span>
+    в будь-який момент (відгуки клієнтів вище).
+  </li>
+  <li className="point">
+    Всі <span style={{ color: "#4824ff" }}>авторські права</span>
+    на роботу переходять замовнику після виконання замовлення.
+  </li>
+  <li className="point">
+    В своїх роботах використовую матеріали строго
+    <span style={{ color: "#4824ff" }}>розрешені</span>
+    для особистого<br /> і комерційного використання.
+  </li>
+</ol>
+    </div>
+}
+
+export default Basement;
