39c90e6171d247c4bb7d18f6cf89e441470c0d4b
bladee <freedum0@gmail.com>
2025-05-05 16:25:59 +0300

    fix icons and text

--- src/component/Basement/Basement.js
+++ src/component/Basement/Basement.js
@@ -1,30 +1,28 @@
 import React from "react";
-import './Basement.css'
+import './Basement.css';
 
 const Basement = () => {
-    <div className="guarantees-block">
-        <h1 style={{ fontSize: "52px", paddingBottom: "20px" }}>ГАРАНТІЇ</h1>
-        <ol className="guarantees-points">
-            <li className="point">
-                Оплату принимаю через платёжную систему <span style={{ color: "#4824ff" }}>Название*</span>, которая контролирует<br />безопасность денежных переводов.
-            </li>
-            <li className="point">
-                Убедиться в моей ответственности и профессионализме можно <span style={{ color: "#4824ff" }}>написав клиентам</span>,<br />оставивших отзывы <span style={{ color: "#4824ff" }}>лично</span> в любой момент (отзывы клиентов выше).
-            </li>
-            <li className="point">
-                Все <span style={{ color: "#4824ff" }}>авторские права</span> на работу переходят заказчику после выполнения заказа.
-            </li>
-            <li className="point">
-                В своих работах использую материалы строго <span style={{ color: "#4824ff" }}>разрешенные для личного<br />и коммерческого использования</span>.
-            </li>
-        </ol>
-            
-        <div className="footer">Workford</div>
-    
-    </div>
+    return (
+        <div className="guarantees-block">
+            <h1 style={{ fontSize: "52px", paddingBottom: "20px" }}>ГАРАНТІЇ</h1>
+            <ol className="guarantees-points">
+                <li className="point">
+                    Оплату приймаю через платіжну систему <span className="highlight">Назва*</span>, яка контролює<br />безпеку грошових переказів.
+                </li>
+                <li className="point">
+                    Переконатися в моїй відповідальності та професіоналізмі можна <span className="highlight">написавши клієнтам</span>,<br />які залишили відгуки <span className="highlight">особисто</span> у будь-який момент (відгуки клієнтів вище).
+                </li>
+                <li className="point">
+                    Всі <span className="highlight">авторські права</span> на роботу переходять замовнику після виконання замовлення.
+                </li>
+                <li className="point">
+                    У своїх роботах використовую матеріали суворо <span className="highlight">дозволені для особистого<br />та комерційного використання</span>.
+                </li>
+            </ol>
 
-    
-
+            <div className="footer">Workford</div>
+        </div>
+    );
 }
 
 export default Basement;
